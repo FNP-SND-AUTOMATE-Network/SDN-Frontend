e0702a5a6c4b4629c7832e89fedb2528e112a49c
Noppadol P. <noppadol.p.promtas@gmail.com>
2025-09-18 19:06:57 +0700

    commit: edit marginleft

--- frontend/src/components/audit/AuditLogFilters.tsx
+++ frontend/src/components/audit/AuditLogFilters.tsx
@@ -65,7 +65,7 @@ export default function AuditLogFilters({
       {/* Filters Modal */}
       {showFilters && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center p-4"
+          className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center p-4" style={{ marginLeft: "0px" }}
           onClick={onToggleFilters}
         >
           <div
